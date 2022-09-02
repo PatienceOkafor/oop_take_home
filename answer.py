fa406dd0dc41bd9dc1e91fa459c5a13a7695d822
PatienceOkafor <ukamakaokafor20@gmail.com>
2022-09-02 11:13:25 +0100

    file with answers

---
+++ answer.py
@@ -0,0 +1,124 @@
+class Cars:
+    model = " "
+    colors = " "
+    year = " "
+    Tyres = " "
+
+Venza = Cars()
+Bmw = Cars()
+Toyota = Cars()
+Lambogini = Cars()
+Ferrari = Cars()
+
+class Person:
+    name = " "
+    age = " "
+    sex = " "
+    height = " "
+
+Patience = Person()
+James = Person()
+Teejah = Person()
+David = Person()
+Onwumi = Person()
+
+class Dog:
+    Name = " "
+    breed = " "
+    color = " "
+
+Rodger = Dog()
+Buzo = Dog()
+Romeo = Dog()
+Kobe = Dog()
+Simba = Dog()
+
+class Laptop:
+    brand = " "
+    color = " "
+    year_manufacture = " "
+
+Hp = Laptop()
+Lenovo = Laptop()
+Asus = Laptop()
+Dell = Laptop()
+Apple = Laptop()
+
+class Shark:
+    animal_type = " "
+    location = " "
+    followers = " "
+
+sammy = Shark()
+philip = Shark()
+stevie = Shark()
+mile = Shark()
+lary = Shark()
+
+class Parrot:
+    species = " "
+    name = " "
+    age = " "
+
+blu = Parrot()
+woo = Parrot()
+roo = Parrot()
+qua = Parrot()
+oot = Parrot()
+
+class Country:
+    name_country = " "
+    capital = " "
+    continent = " "
+
+Nigeria = Country()
+Cameroon = Country()
+India = Country()
+Sweden = Country()
+Britain = Country()
+
+class Employee:
+    emp_name = " "
+    salary = " "
+    Gender = " "
+
+Mary = Employee()
+john = Employee()
+Sandra = Employee()
+Paul = Employee()
+Peace = Employee()
+
+class Food:
+    type = " "
+    taste = " "
+    color = " "
+
+Rice = Food()
+stew = Food()
+beans = Food()
+spaghetti = Food()
+charwama = Food()
+
+class Pet:
+    choice = " "
+    color = " "
+    movement = " "
+
+dog = Pet()
+cat = Pet()
+python =  Pet()
+guinnea_pig = Pet()
+parrot = Pet()
+
+
+class Female:
+    firstname = ""
+    age = ""
+    sex = ""
+    height = ""
+
+Patient = Female()
+Jamefine = Female()
+Teejahdys = Female()
+Daviys = Female()
+Onwumyyy = Female()s
